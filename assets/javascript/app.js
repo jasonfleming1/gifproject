8574765e5e8d4ddfd1727d46d23a87dcbda2aacb
phantom <phantomdesign9@gmail.com>
2019-03-05 23:02:01 -0600

    working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
 
     //make an array of defaults
-    var movies = ["terminator", "strange brew", "die hard", "pee wee herman", "star wars", "andre the giant", "david hasslehoff"];
+    var movies = ["eric andre", "strange brew", "pee wee herman", "keanu", "andre the giant", "david hasslehoff", "cheech and chong", "scooby", "darth vader"];
     console.log(movies);
     //Function to display array as buttons
     function getButtons() {
@@ -25,6 +25,65 @@ $(document).ready(function () {
             getButtons();
         });
     }
+
+    //api call function
+    function getGifs() {
+        var searchValue = $(this).attr("data-name");
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + searchValue + "&api_key=3hZSWMaSjJSWno43Pmp7OILkjlhl4wmX&limit=10"
+        console.log(queryURL);
+
+        $.ajax({
+            url: queryURL,
+            method: 'GET'
+        })
+            .then(function (response) {
+
+                $("#showGif").empty();
+
+                var results = response.data;
+                if (results == "") {
+                    alert("Nothing to see here...try again!")
+                }
+                for (var i = 0; i < results.length; i++) {
+                    var gifDiv = $("<div>");
+                    gifDiv.addClass("gifDiv");
+                    gifDiv.addClass("card");
+                    var image = $("<img>");
+                    image.attr("src", results[i].images.fixed_height_still.url);
+                    image.attr("data-still", results[i].images.fixed_height_still.url);
+                    image.attr("data-animate", results[i].images.fixed_height.url);
+                    image.attr("data-state", "still");
+                    image.addClass("image");
+                    image.addClass("card-img-top");
+                    gifDiv.append(image);
+
+                    var rating = $("<p>").text("Rating: " + results[i].rating);
+                    rating.addClass("card-text");
+                    gifDiv.append(rating);
+
+                    $("#showGif").prepend(gifDiv);
+                }
+
+            })
+    }
+
+    //here we are calling the functions
     getButtons();
     newButton();
+
+    //click to get the gifs
+    $(document).on("click", ".addMovie", getGifs);
+
+    //click to animate the gifs
+    $(document).on("click", ".image", function() {
+        var currentState = $(this).attr('data-state');
+        if (currentState = 'still') {
+            $(this).attr('src', $(this).data("animate"));
+            $(this).attr('data-state', 'animate');
+        }
+        else {
+            $(this).attr('src', $(this).data('still'));
+            $(this).attr('data-state', 'still');
+        }
+    })
 });
